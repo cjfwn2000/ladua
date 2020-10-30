fadc361525a6278d4673bcbe62f3c37041507024
FeAchaleen <feachaleen@gmail.com>
2020-10-30 17:22:26 +0900

    study contd

---
+++ source/tdinterface.c
@@ -0,0 +1,33 @@
+#include "tdinterface.h"
+
+#include <errno.h>  // Error number
+#include <fcntl.h>   // File control
+#include <termios.h>  // POSIX terminal control 
+#include <unistd.h>  // Unic standard function 
+#include <stdio.h>
+#include <string.h>
+
+// -- Private functions ----------------
+
+
+
+// -- Public functions -----------------
+
+int TDI_open(TDInterface * tdi)
+{
+    char portname[] = "/dev/ttyACM0";
+    int fd;  //file descriptor to the serial port
+
+    // Opening
+    fd = open(portname, O_RDWR | O_NOCTTY | O_SYNC);
+    if (fd < 0) {  //could not open the serial port
+        fprintf(stderr, "Error opening the serial port.\n");
+        return -1;
+    }
+    // Setting the interface attributes (e.g. baud rate)
+    if(_setInterfaceAttr(fd)) {
+        fprintf(stderr, "Failed setInterfaceAttr.\n");
+    }
+}
+
+int TDI_read(TDInterface * tdi, char * bigbuf);
